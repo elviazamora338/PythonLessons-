17e2a38cfeacf71dc4ff6129c33a541f7894a692
elviazamora338 <elviazamora338@gmail.com>
2024-12-28 14:59:07 -0600

    new

--- basic.py
+++ basic.py
@@ -1,4 +1,17 @@
 lara = "He don't know"
 
+# int variable
+num = 1
 
-print(lara)
+# double or float variable
+num2 = 1.3
+
+# declaring a function 
+def firstFunction(test):
+    print(test + " doesn't know")
+
+# call the function
+
+firstFunction("Lara")
+
+print(num)
